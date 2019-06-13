063e1b0388adaacf2b2f00b79e8c14a9c4700f05
RonanV <ronan@ronan-pc.home>
2019-06-13 18:45:57 +0200

    ajout securite

---
+++ ApiSpring/src/main/java/com/example/apispring/security/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.example.apispring.security;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers("/articles/all").permitAll()
+		.and()
+			.formLogin()
+		.and()
+			.logout()
+		.and()
+			.authorizeRequests().anyRequest().authenticated();			
+		
+	}
+	
+}
+
